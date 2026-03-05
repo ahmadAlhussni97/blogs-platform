0a2f391b7ac238c828dddffafd9f3cd30bfa3a8d
Ahmad Alhussni <ahmadalhussni2018@gmail.com>
2026-03-05 17:51:15 +0400

    update pagiantion

--- src/components/Pages/Home/Pagination.jsx
+++ src/components/Pages/Home/Pagination.jsx
@@ -1,53 +1,63 @@
-
-import { useState,memo,useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import {constData} from '../../../Helpers/ConstData'
+import { useState, memo, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { constData } from "../../../Helpers/ConstData";
 
 export default memo(function Pagination(props) {
+  const { start, limit, length, changePage } = props;
 
-  const [currentPage,setCurrentPage] = useState((props.start>0)?parseInt(props.start/10):0)
-  const recordsPerPage = props.limit
-  // Records to be displayed on the current page
-  const nPages = Math.ceil(props.length / recordsPerPage)  
-  const pageNumbers = [...Array(nPages + 1).keys()].slice(1)
+  const [currentPage, setCurrentPage] = useState(
+    start > 0 ? parseInt(start / 10) : 0
+  );
+  const recordsPerPage = limit;
+  const nPages = Math.ceil(length / recordsPerPage);
+  const pageNumbers = [...Array(nPages + 1).keys()].slice(1);
 
+  const reInitialize = () => {
+    if (Number(start) <= 0) setCurrentPage(constData().defultStart);
+  };
 
-  const reInitialize=()=>{
-    if(eval(props.start) <= 0)
-      setCurrentPage(constData().defultStart)
-  }
-
-  const generateNewPage=(pgNumber)=>{
-     setCurrentPage(pgNumber)
-  }
+  const generateNewPage = (pgNumber) => {
+    setCurrentPage(pgNumber);
+  };
 
   const goToNextPage = () => {
-    if(currentPage !== (nPages -1))
-      setCurrentPage(currentPage + 1)
-  }
-  
+    if (currentPage !== nPages - 1) setCurrentPage(currentPage + 1);
+  };
+
   const goToPrevPage = () => {
-    if(currentPage !== 0)
-      setCurrentPage(currentPage - 1)
-  }
+    if (currentPage !== 0) setCurrentPage(currentPage - 1);
+  };
 
-  useEffect(()=>{
-    props.changePage(currentPage)
-  },[currentPage])
+  // Call changePage whenever currentPage changes
+  useEffect(() => {
+    changePage(currentPage);
+  }, [currentPage]);
 
-
-  useEffect(()=>{
-    reInitialize()
-  },[props.start])
+  // Re-initialize whenever start changes
+  useEffect(() => {
+    reInitialize();
+  }, [start]);
 
   return (
     <div className="pagination">
-       <Link onClick={()=>goToPrevPage()}  to="/" >«</Link>
-       {pageNumbers.map(pgNumber => (
-        <Link  key={pgNumber-1}  className= {`page-item ${currentPage === (pgNumber-1) ? 'active-link' : ''} `} 
-        onClick={()=>generateNewPage(pgNumber-1)} to="/" >  {pgNumber}  </Link>
-         ))}
-        <Link onClick={()=>goToNextPage()} to="/" >»</Link>
+      <Link onClick={goToPrevPage} to="/">
+        «
+      </Link>
+      {pageNumbers.map((pgNumber) => (
+        <Link
+          key={pgNumber - 1}
+          className={`page-item ${
+            currentPage === pgNumber - 1 ? "active-link" : ""
+          } `}
+          onClick={() => generateNewPage(pgNumber - 1)}
+          to="/"
+        >
+          {pgNumber}
+        </Link>
+      ))}
+      <Link onClick={goToNextPage} to="/">
+        »
+      </Link>
     </div>
-  )
-})
+  );
+});
